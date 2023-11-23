ef8cfc01fd5b496143c6671a3bc588ddd93d713e
Kodama Naoyuki <mmaga@dstribe.net>
2023-11-23 09:39:56 +0900

    refactor: Remove unnecessary null type

--- src/main/kotlin/net/dstribe/customize_word_separators/settings/AppSettingsComponent.kt
+++ src/main/kotlin/net/dstribe/customize_word_separators/settings/AppSettingsComponent.kt
@@ -29,7 +29,7 @@ class AppSettingsComponent {
         return mainPanel
     }
 
-    fun getPreferredFocusedComponent(): JComponent? {
+    fun getPreferredFocusedComponent(): JComponent {
         return customizedPatterns
     }
 
