e0e6e6bddd45be03ec53d8a0c614a8718e8e05c6
Kodama Naoyuki <mmaga@dstribe.net>
2024-09-26 09:44:11 +0900

    build: update Kover plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,10 +62,12 @@ qodana {
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-koverReport {
-    defaults {
-        xml {
-            onCheck = true
+kover {
+    reports {
+        total {
+            xml {
+                onCheck = true
+            }
         }
     }
 }
